a51bbd5d880a6ba1f04b2431b6132819af25dc38
antizun <ricardo.f.zuniga@gmail.com>
2022-01-28 18:19:00 -0300

    update

--- flask_auth_app/project/main.py
+++ flask_auth_app/project/main.py
@@ -92,7 +92,7 @@ def my_form_post():
         
     
     arduino = serial.Serial('/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 9600)
-    char comando='H'
+    comando=str('H')
     arduino.write(comando.encode())
 	
     arduino.close() #Finalizamos la comunicacion    
