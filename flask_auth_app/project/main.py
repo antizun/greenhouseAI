82a5bc3bb5dc01de99a5d18ff948e66dbd2ea399
antizun <ricardo.f.zuniga@gmail.com>
2022-01-28 16:29:46 -0300

    update

--- flask_auth_app/project/main.py
+++ flask_auth_app/project/main.py
@@ -109,12 +109,12 @@ def my_form_post():
     comando='H'
     arduino.write(comando.encode())
     arduino.close() #Finalizamos la comunicacion     
-    if (request.form['riego_manual']=='RIEGO_MANUAL'):
+    if (request.form['riego_manual']=='riego_manual'):
         print('RIEGO MANUAL')
         comando='H'
         arduino.write(comando.encode())
         arduino.close() #Finalizamos la comunicacion
-    if (request.form['ventilar']=='VENTILAR'):
+    if (request.form['ventilar']=='ventilar'):
         print('VENTILAR')
         comando='H'
         arduino.write(comando.encode())
