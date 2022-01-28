8174a3cf9c0051107356e4d64dc6c0101aafbbb4
antizun <ricardo.f.zuniga@gmail.com>
2022-01-28 18:31:57 -0300

    update

--- flask_auth_app/project/main.py
+++ flask_auth_app/project/main.py
@@ -91,12 +91,13 @@ def my_form_post():
 
         
     
-    arduino = serial.Serial('/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 9600)
-    comando=str('H')
-    arduino.write(comando.encode())
 
-    arduino.close() #Finalizamos la comunicacion    
-    print(request.form['riego_manual']) 
+    
+    if (request.form['riego_manual']) is not None:
+       arduino = serial.Serial('/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 9600)
+       comando=str('H')
+       arduino.write(comando.encode())
+       arduino.close() #Finalizamos la comunicacion    
     # if (request.form['riego_manual']=='riego_manual'):
     #     print('RIEGO MANUAL')
     #     comando='H'
