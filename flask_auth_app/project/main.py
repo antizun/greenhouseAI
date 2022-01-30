87581c300a0b305f1ef74a6090db1ecfceb3636e
antizun <ricardo.f.zuniga@gmail.com>
2022-01-29 23:07:26 -0300

    update

--- flask_auth_app/project/main.py
+++ flask_auth_app/project/main.py
@@ -104,15 +104,15 @@ def my_form_post():
 
     print('Running. Press CTRL-C to exit.')
     with serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 9600, timeout=1) as arduino:
-        time.sleep(0.05) #wait for serial to open
+        #time.sleep(0.05) #wait for serial to open
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
             try:
                 while True:
-                    cmd='RIEGO_MANUAL'
+                    cmd='R'
                     #cmd=input("Enter command : ")
                     arduino.write(cmd.encode())
-                    time.sleep(0.05) #wait for arduino to answer
+                    #time.sleep(0.05) #wait for arduino to answer
                     while arduino.inWaiting()==0: pass
                     if  arduino.inWaiting()>0: 
                         answer=arduino.readline()
