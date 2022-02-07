12629bb7ce97ff727112de7d4fb32dcc23fec1bb
antizun <ricardo.f.zuniga@gmail.com>
2022-02-07 02:54:45 -0300

    update

--- DHT22_Sensor/logDHT.py
+++ DHT22_Sensor/logDHT.py
@@ -16,7 +16,7 @@ def getDHTdata():
 	ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", baudrate=9600)
 	try:
 		while True:
-			cmd = 't'
+			cmd = "t"
 			comandoBytes = cmd.encode()
 			ser.write(comandoBytes)
 			time.sleep(0.1)
