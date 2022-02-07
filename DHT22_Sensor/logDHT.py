2ed9437c98e5ba10ce89b0fbe4ac53ad1a33ce19
antizun <ricardo.f.zuniga@gmail.com>
2022-02-06 22:01:15 -0300

    update

--- DHT22_Sensor/logDHT.py
+++ DHT22_Sensor/logDHT.py
@@ -10,6 +10,50 @@ def getDHTdata():
 	DHT22Sensor = Adafruit_DHT.DHT22
 	DHTpin = 16
 	#hum, temp = Adafruit_DHT.read_retry(DHT22Sensor, DHTpin)
+
+	with serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 9600, timeout=1) as arduino:
+		time.sleep(0.1) #wait for serial to open
+		if arduino.isOpen():
+			print("{} connected!".format(arduino.port))
+			try:
+				#while True:
+					cmd='TEMP'
+					cmd=input("Enter command : ")
+					arduino.write(cmd.encode())
+					time.sleep(2) #wait for arduino to answer
+					while arduino.inWaiting()==0: pass
+					if  arduino.inWaiting()>0: 
+						answer=arduino.readline()
+						print(answer)
+						arduino.flushInput() #remove data after reading
+						#break
+
+					cmd='HUM'
+					cmd=input("Enter command : ")
+					arduino.write(cmd.encode())
+					time.sleep(2) #wait for arduino to answer
+					while arduino.inWaiting()==0: pass
+					if  arduino.inWaiting()>0: 
+						answer=arduino.readline()
+						print(answer)
+						arduino.flushInput() #remove data after reading
+						#break
+
+					cmd='HGR'
+					cmd=input("Enter command : ")
+					arduino.write(cmd.encode())
+					time.sleep(2) #wait for arduino to answer
+					while arduino.inWaiting()==0: pass
+					if  arduino.inWaiting()>0: 
+						answer=arduino.readline()
+						print(answer)
+						arduino.flushInput() #remove data after reading
+						#break
+
+			except KeyboardInterrupt:
+				print("KeyboardInterrupt has been caught.")
+
+
 	hum=22.1
 	temp=33.3
 	if hum is not None and temp is not None:
