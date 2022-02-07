2a5b67df256412bc877c1e7a7b380ca774a4e2f2
antizun <ricardo.f.zuniga@gmail.com>
2022-02-07 02:21:09 -0300

    update

--- DHT22_Sensor/logDHT.py
+++ DHT22_Sensor/logDHT.py
@@ -13,37 +13,25 @@ hgr=1.1
 
 def getDHTdata():	
 
-	global temp, hum, hgr
-
-	arduino=serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 9600, timeout=1)
-
-	time.sleep(0.1) #wait for serial to open
-	if arduino.isOpen():
-		print("{} connected!".format(arduino.port))
-
-		cmd='t'
-		#cmd=input("Enter command : ")
-		arduino.write(cmd.encode())
-		time.sleep(2) #wait for arduino to answer
-		# while arduino.inWaiting()==0: pass
-		# if  arduino.inWaiting()>0: 
-		answer=arduino.readline()
-		temp=answer.decode("utf-8")
-		print(answer.decode("utf-8"))
-		arduino.flushInput() #remove data after reading
-
-		# answer=arduino.readline()
-		# hum=answer.decode("utf-8")
-		# print(answer.decode("utf-8"))
-		# arduino.flushInput() #remove data after reading
-
-		# answer=arduino.readline()
-		# hgr=answer.decode("utf-8")
-		# print(answer.decode("utf-8"))
-
-		# arduino.flushInput() #remove data after reading
-
-		arduino.close()
+	print('Running. Press CTRL-C to exit.')
+	with serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 9600, timeout=1) as arduino:
+		time.sleep(0.1) #wait for serial to open
+		if arduino.isOpen():
+			print("{} connected!".format(arduino.port))
+			try:
+				#while True:
+					cmd='t'
+					#cmd=input("Enter command : ")
+					arduino.write(cmd.encode())
+					time.sleep(2) #wait for arduino to answer
+					while arduino.inWaiting()==0: pass
+					if  arduino.inWaiting()>0: 
+						answer=arduino.readline()
+						print(answer)
+						arduino.flushInput() #remove data after reading
+						#break
+			except KeyboardInterrupt:
+				print("KeyboardInterrupt has been caught.")
 
 
 
